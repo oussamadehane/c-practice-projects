3184236d68c2369abd6d86a88f3d20207c7f687f
oussamadehane <139594051+oussamadehane@users.noreply.github.com>
2026-06-23 22:23:42 +0100

    Add welcome message printing functionality

---
+++ welcome_message.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_welcome_message - Prints the welcome message to the standard output.
+ */
+void print_welcome_message(void) 
+{
+    printf("Welcome to Gammal Tech's International Printf Introduction!\n");
+}
+
+int main(void) 
+{
+    // استدعاء الدالة الخاصة بالترحيب
+    print_welcome_message();
+
+    return 0;
+}
