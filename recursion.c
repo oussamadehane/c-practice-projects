f264ba4fa890647a1672053bd0e96b60a76bb7cc
oussamadehane <139594051+oussamadehane@users.noreply.github.com>
2026-07-09 20:42:01 +0100

    Add recursion functionality to print numbers
    Implement recursive function to print numbers from input down to zero.

---
+++ recursion.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int ReadNumber()
+{
+    int Number = 0;
+    printf("Please Enter A Number: ");
+
+    // التحقق من نجاح عملية القراءة
+    while (scanf("%d", &Number) != 1)
+    {
+        printf("Invalid input! Please enter a valid number: ");
+        // تنظيف مدخلات الذاكرة المؤقتة من أي رموز خاطئة
+        (void)scanf("%*[^\n]");
+        (void)scanf("%*c");
+    }
+    return Number;
+}
+
+
+void Recursion(int Number)
+{
+    //  Base Case;
+    if (Number == 0)return;
+    printf("%d", Number);
+    Recursion(Number - 1);
+}
+
+
+int main() {
+   
+    Recursion(ReadNumber());
+    return 0;
+}
